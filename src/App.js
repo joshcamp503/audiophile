00dc4f918c738a3ad166913ff9089861700f4aaf
joshcamp503 <joshcamp503@gmail.com>
2022-03-27 02:14:36 -0700

    simplified url and repo name

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/audiophile">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
